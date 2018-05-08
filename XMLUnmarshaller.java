4afbc3b863619ef61354d58d962c0b87fc265205
Raimsten <higonet.r@gmail.com>
2018-05-08 15:52:18 -0300

    scripts para XML pasar de objeto a xml viceverse

---
+++ XMLUnmarshaller.java
@@ -0,0 +1,25 @@
+package com.mkyong.core;
+
+import java.io.File;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+public class JAXBExample2 {
+	public static void main(String[] args) {
+
+	 try {
+
+		File file = new File("file.xml");
+		JAXBContext jaxbContext = JAXBContext.newInstance(Customer.class);
+
+		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+		Customer customer = (Customer) jaxbUnmarshaller.unmarshal(file);
+		System.out.println(customer);
+
+	  } catch (JAXBException e) {
+		e.printStackTrace();
+	  }
+
+	}
+}
